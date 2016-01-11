55db0e2fbd3bf80cef900a043b6dcda2342874a6
tsgardner <hsmertk@gmail.com>
2016-01-11 04:36:33 +0200

    Initialized BE solver function.

--- solve_by_be.m
+++ solve_by_be.m
@@ -3,8 +3,8 @@ function m = solve_by_be(f,tRange,initVal)
 m    = zeros(size(tRange));
 m(1) = initVal;
 
-for kk = 2:numel(tRange)
-    m(kk) = m(kk-1) + ( tRange(kk) - tRange(kk-1) ) * f( tRange(kk-1) , m(kk-1) );
+for kk = 1:(numel(tRange)-1)
+    m(kk+1) = m(kk) + ( tRange(kk+1) - tRange(kk) ) * f( tRange(kk) , m(kk) );
 end
 
 end % function
